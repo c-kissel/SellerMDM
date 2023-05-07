2cf94721604c7db86e5ec543c9e0ddaf400320ce
Constantine Kissel <kissel@me.com>
2023-05-07 12:05:05 +0500

    Remove create database from 1 migration step

--- internal/storage/db/postgres/migrations/000001_init.up.sql
+++ internal/storage/db/postgres/migrations/000001_init.up.sql
@@ -1,10 +1,10 @@
 -- Database: smdm
-CREATE DATABASE smdm
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+-- CREATE DATABASE smdm
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'en_US.utf8'
+--     LC_CTYPE = 'en_US.utf8'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
